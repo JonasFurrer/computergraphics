52de2475d206221f968cfd982dc2283a1a562a27
Aless55 <aless1997@windowslive.com>
2018-10-31 16:19:19 +0100

    added all functionalities

---
+++ OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/bezier.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/bezier.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/billboard.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/billboard.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/glfw_window.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/glfw_window.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/glmath.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/glmath.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/main.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/main.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/shader.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/shader.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/ship.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/ship.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/solar_viewer.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/solar_viewer.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/sphere.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/sphere.cpp.o"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src/texture.cpp" "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/src/CMakeFiles/SolarSystem.dir/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "SHADER_PATH=\"/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/src\""
+  "TEXTURE_PATH=\"/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/textures\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/glfw/include"
+  "../src"
+  "../lib/lodePNG"
+  "/usr/include/GL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/lib/lodePNG/CMakeFiles/lodePNG.dir/DependInfo.cmake"
+  "/home/alessandro/Dokumente/Übungen/computergrafik/computergraphics/OpenGL/assignment_5/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
